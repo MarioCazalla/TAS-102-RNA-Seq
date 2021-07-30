8e0c563222293e952dadb7cd602de68ed3322a6d
MarioCazalla <cazaleon16@hotmail.com>
2021-07-30 13:39:06 +0200

    Update

--- differential_expression.R
+++ differential_expression.R
@@ -1,10 +1,11 @@
+## Install BiocManager if not installed ##
+
 # if (!requireNamespace("BiocManager", quietly = TRUE))
 #   install.packages("BiocManager")
 # BiocManager::install(version = "3.13")
-# install.packages("readxl")
+## Install packages if not installed ##
+#BiocManager::install(c("EnhancedVolcano", "ReactomePA", "data.table", "dplyr", "DESeq2", "ggplot2", "org.Hs.eg.db", "AnnotationDbi", "clusterProfiler", "ggbeeswarm", "volcano3D"))
 
-# BiocManager::install(c("EnhancedVolcano", "ReactomePA", "data.table", "dplyr", "DESeq2", "ggplot2", "org.Hs.eg.db", "AnnotationDbi", "clusterProfiler", "ggbeeswarm", "volcano3D"))
-#install.packages("countToFPKM")
 library(data.table)
 library(DESeq2)
 library(ggplot2)
@@ -18,17 +19,17 @@ library(ReactomePA)
 library(EnhancedVolcano)
 
 #library(volcano3D)
-data(example_data)
-######  SNU-C4 #####
+
+##### SNU-C4 #####
 setwd("E:\\IRBLLEIDA/RNA-Sequencing/alignment/SNU-C4/")
 ##### LS513 #####
 setwd("E:\\IRBLLEIDA/RNA-Sequencing/alignment/LS513/")
 ##### LIM2099 #####
 setwd("E:\\IRBLLEIDA/RNA-Sequencing/alignment/LIM-2099//")
 
-#Preparamos la matriz de analisis para DESeq2
+### Preparamos la matriz de analisis para DESeq2 ###
 
-######  SNU-C4 #####
+##### SNU-C4 #####
 Rmatrix <- as.matrix(read.csv("Rmatrix_SNUC4.txt", header = TRUE, sep = "\t", row.names = "Geneid"))
 colnames(Rmatrix) <- c("RNA-29", "RNA-30", "RNA-31", "RNA-32") 
 metaData <- as.matrix(read.csv("metaData_SNUC4.csv", header = TRUE, sep = "\t"))
@@ -43,122 +44,105 @@ Rmatrix <- as.matrix(read.csv("Rmatrix_LIM2099.txt", header = TRUE, sep = "\t", 
 colnames(Rmatrix) <- c("RNA-25", "RNA-26", "RNA-27", "RNA-28") 
 metaData <- as.matrix(read.csv("metaData_LIM2099.csv", header = TRUE, sep = "\t"))
 
-### Resistant-Sensible MasterTable ###
-setwd("C:\\Users/usuari/Desktop")
-Rmatrix <- as.matrix(read.csv("Rmatrix.txt", header = TRUE, sep = "\t", row.names = "Geneid"))
-metaData <- as.matrix(read.csv("metaData.txt", header = TRUE, sep = "\t"))
+### 6 vs 6 MasterTable ### Analisis de las 6 lineas celulares de cada extremo.
+setwd("C:\\Users/usuari/Desktop/TAS-102-RNA-Seq/DEG_Master_Table/6vs6/")
+Rmatrix <- as.matrix(read.csv("Rmatrix6vs6.txt", header = TRUE, sep = "\t", row.names = "Geneid"))
+metaData <- as.matrix(read.csv("metaData6vs6.txt", header = TRUE, sep = "\t"))
 
+### 6 vs Rest MasterTable ### Analysis of 6 cell lines of the right side of histogram vs ALL ###
+setwd("C:\\Users/usuari/Desktop/TAS-102-RNA-Seq/DEG_Master_Table/6vsALL/")
+Rmatrix <- as.matrix(read.csv("Rmatrix6vsALL.txt", header = TRUE, sep = "\t", row.names = "Geneid"))
+metaData <- as.matrix(read.csv("metaData6vsALL.txt", header = TRUE, sep = "\t"))
 
-dim(Rmatrix)
+###############
 
 Rmatrix1 <- na.omit(Rmatrix)
 dim(Rmatrix1)
-
 dds <- DESeqDataSetFromMatrix(countData = Rmatrix1,
                               colData = metaData,
                               design = ~dex, tidy = FALSE)
-#Con esto nos quedamos con los genes que tienen mas de 1 counts. Disminuimos matriz
+
+### This command let us to save genes with more than 1 count, matrix is smaller ###
 keep <- rowSums(counts(dds)) > 1
 dds <- dds[keep,]
-# nrow(dds)
 
-#DESeq2
-
+### Run DESeq2 ###
 dds <- DESeq(dds)
 res <- results(dds)
 head(results(dds, tidy=TRUE))
 
-#rm(keep, metaData, Rmatrix, dds)
-#Observamos los resultados
-
-summary(res)
-#Ordenamos por p.adj
+### Sort by p.adj ###
 res <- res[order(res$padj), ]
-#Vemos los resultados
 head(res)
 
-# sum(res$pvalue < 0.05, na.rm = TRUE)
-# sum(!is.na(res$pvalue))
-# sum(res$padj < 0.1, na.rm = TRUE)
-
-#After multiple testing: genes with padj < 0.2 
+### Filtering: multiple test correction: genes with padj < 0.2 ###
 resSig <- subset(res, padj < 0.2)
+head(resSig[ order(resSig$log2FoldChange, decreasing = FALSE), ]) #To see results
 
 #### Calcular el porcentaje de genes que han cambiado tras el tratamiento
 
 
 
-#Vemos los resultados
-head(resSig[ order(resSig$log2FoldChange), ])
-head(resSig[ order(resSig$log2FoldChange, decreasing = FALSE), ])
+### Save dataframe wit gene name, log2FC, pvalue and padj
+resSig_log2FC <- as.data.frame(cbind(resSig@rownames, resSig$log2FoldChange, resSig$pvalue, resSig$padj))
+colnames(resSig_log2FC) <- c("GeneName", "log2FC", "pvalue", "padj")
+write.table(resSig_log2FC, file = "genename_log2FC.csv", sep = "\t", row.names = FALSE) #Next step: convert "." to "," in excel to treat them like numbers, not like text.
 
-#Before multiple testing: genes with pvalue < 0.05
-resSIG <- subset(res, pvalue < 0.05)
-#Vemos los resultados
-head(resSIG[order(resSIG$log2FoldChange, decreasing = TRUE), ])
-
-#Guardamos gene name con log2FC y pvalue/padj
-
-resSIG_log2FC <- as.data.frame(cbind(resSIG@rownames, resSIG$log2FoldChange, resSIG$pvalue, resSIG$padj))
-colnames(resSIG_log2FC) <- c("GeneName", "log2FC", "pvalue", "padj")
-write.table(resSIG_log2FC, file = "genename_log2FC.csv", sep = "\t", row.names = FALSE)
-
-#Ahora filtraremos por log2FoldChange para ver cuales son UP o DOWN regulated
-  #ADJUSTED P-VALUE
+### Now, we are filtering by log2FoldChange to clasify which are up or down regulated.
+  #Using the data that pass multiple test correction (ADJUSTED P-VALUE < 0.02)
 log2FC_UP <- subset(resSig, resSig$log2FoldChange > 1.5) 
 log2FC_UP <- log2FC_UP[order(log2FC_UP$log2FoldChange, decreasing = TRUE), ]
 log2FC_DOWN <- subset(resSig, resSig$log2FoldChange < -1.5) 
 log2FC_DOWN <- log2FC_DOWN[order(log2FC_DOWN$log2FoldChange, decreasing = FALSE), ]
 
+#We save the downregulated gene names with their log2fc.
 name_log2fc_down <- as.data.frame(log2FC_DOWN@rownames)
 colnames(name_log2fc_down) <- "GeneName"
 name_log2fc_down$log2fc=log2FC_DOWN$log2FoldChange
+write.table(name_log2fc_down, file = "name_log2FC_down.csv", sep = "\t", row.names = FALSE)
 
+#We save the upregulated gene names with their log2fc.
 name_log2fc_up <- as.data.frame(log2FC_UP@rownames)
 colnames(name_log2fc_up) <- "GeneName"
 name_log2fc_up$log2fc=log2FC_UP$log2FoldChange
+write.table(name_log2fc_up, file = "name_log2FC_up.csv", sep = "\t", row.names = FALSE)
 
+### We keep gene names
 resSig_names <- resSig@rownames
 resSig_up_names <- log2FC_UP@rownames
 resSig_down_names <- log2FC_DOWN@rownames
 
-  #P-VALUE
+### Before multiple test correction: genes with pvalue < 0.05
+resSIG <- subset(res, pvalue < 0.05)
+head(resSIG[order(resSIG$log2FoldChange, decreasing = TRUE), ])
+
+  #Using the data that pass P-VALUE < 0.05
 log2FC_UP_pv <- subset(resSIG, resSIG$log2FoldChange > 0) 
 log2FC_UP_pv <- log2FC_UP_pv[order(log2FC_UP_pv$log2FoldChange, decreasing = TRUE), ]
 log2FC_DOWN_pv <- subset(resSIG, resSIG$log2FoldChange < 0) 
 log2FC_DOWN_pv <- log2FC_DOWN_pv[order(log2FC_DOWN_pv$log2FoldChange, decreasing = FALSE), ]
 
-resSIG_names <- resSIG@rownames
-no_LOC <- resSIG[!grepl("^LOC", resSIG@rownames),]
+#We save the downregulated gene names with their log2fc.
+name_log2fc_down_pv <- as.data.frame(log2FC_DOWN_pv@rownames)
+colnames(name_log2fc_down_pv) <- "GeneName"
+name_log2fc_down_pv$log2fc=log2FC_DOWN_pv$log2FoldChange
+write.table(name_log2fc_down_pv, file = "name_log2FC_down.csv", sep = "\t", row.names = FALSE)
 
+#We save the upregulated gene names with their log2fc.
+name_log2fc_up_pv <- as.data.frame(log2FC_UP_pv@rownames)
+colnames(name_log2fc_up_pv) <- "GeneName"
+name_log2fc_up_pv$log2fc=log2FC_UP_pv$log2FoldChange
+write.table(name_log2fc_up_pv, file = "name_log2FC_up.csv", sep = "\t", row.names = FALSE)
+
+### We keep gene names
+resSIG_names <- resSIG@rownames
 resSIG_UP_names <-log2FC_UP_pv@rownames
 resSIG_DOWN_names <- log2FC_DOWN_pv@rownames
 
+### To remove the gene names LOC... ### 
+#no_LOC <- resSIG[!grepl("^LOC", resSIG@rownames),]
 
-ensembl_matrix <- fread("ensembl_gsea.txt", header = FALSE, sep = "\t" )
-ENSEMBL <- AnnotationDbi::select(org.Hs.eg.db,
-                                     keys = ensembl_matrix$V1, #Cambiar key segun analisis
-                                     keytype = "SYMBOL",
-                                     columns = "ENSEMBL")
-ENSEMBL <- ENSEMBL[!is.na(ENSEMBL$ENSEMBL), ]
-write.table(ENSEMBL, file = "ensembl_id.csv", sep = "\t", row.names = FALSE)
-
-
-#### UNIPROT ####
-
-UNIPROT_IDs <- AnnotationDbi::select(org.Hs.eg.db,
-                                     keys = no_LOC@rownames, #Cambiar key segun analisis
-                                     keytype = "SYMBOL",
-                                     columns = "ENSEMBL")
-UNIPROT_IDs <- UNIPROT_IDs[!is.na(UNIPROT_IDs$UNIPROT), ]
-#UNIPROT_IDs <- UNIPROT_IDs[, "UNIPROT"]
-write.table(UNIPROT_IDs, file = "uniprots_id.csv", sep = ",", row.names = FALSE, col.names = FALSE)
-
-
-
-
-
-#Guardamos los resultados 
+### To keep the results in the directory ###
 write.table(resSig_names, file = "diff_expressed_padj_genenames.csv", sep = "\t", row.names = FALSE, col.names = FALSE)
 write.table(resSig_down_names, file = "down_regulated_padj_names.csv", sep = "\t", row.names = FALSE, col.names = FALSE)
 write.table(resSig_up_names, file = "up_regulated_padj_names.csv", sep = ",", row.names = FALSE, col.names = FALSE)
@@ -167,72 +151,39 @@ write.table(resSIG_names, file = "diff_expressed_pval__genenames.csv", sep = "\t
 write.table(resSIG_DOWN_names, file = "down_regulated_pval_names.csv", sep = "\t", row.names = FALSE, col.names = FALSE)
 write.table(resSIG_UP_names, file = "up_regulated_pval_names.csv", sep = ",", row.names = FALSE, col.names = FALSE)
 
-#### CYTOSCAPE ####
-cytoscape <- as.data.frame(resSIG@rownames)
+#### UNIPROT ID CONVERSION FROM x ####
+
+UNIPROT_IDs <- AnnotationDbi::select(org.Hs.eg.db,
+                                     keys = resSig@rownames, #Change Keys when you change the analysis
+                                     keytype = "SYMBOL",
+                                     columns = "ENSEMBL") #Change Keys when you change the analysis
+UNIPROT_IDs <- UNIPROT_IDs[!is.na(UNIPROT_IDs$UNIPROT), ]
+write.table(UNIPROT_IDs, file = "uniprots_id.csv", sep = ",", row.names = FALSE, col.names = FALSE)
+
+#### CYTOSCAPE dataframe Preparation ####
+cytoscape <- as.data.frame(resSig@rownames)
 colnames(cytoscape) <- "Gene_Name"
 entrez_ids <- AnnotationDbi::select(org.Hs.eg.db,
-                                    keys = resSIG@rownames, #Cambiar key segun analisis
-                                    keytype = "SYMBOL",
-                                    columns = "ENSEMBL")
-entrez_ids <- entrez_ids[-c(21,29),] #Porque se repite y hay que eliminarla
-cytoscape$GeneID=entrez_ids$ENSEMBL
-cytoscape$log2FC=resSIG$log2FoldChange
-cytoscape$pvalue=resSIG$pvalue
-cytoscape$Gene_Name <- NULL
-cytoscape <- cytoscape[!is.na(cytoscape$GeneID), ] 
-
-#setwd("C:\\Users/usuari/Desktop")
-#write.table(entrez_ids, file = "entrez_id", sep = "\t", row.names = FALSE, col.names = FALSE)
-write.table(cytoscape, file = "cytoscape_ENSG.csv", sep = "\t", row.names = FALSE)
-
-
-### REACTOMEPA ###
-no_LOC <- resSIG[!grepl("^LOC", resSIG@rownames),]
-
-eg = bitr(no_LOC@rownames, fromType="SYMBOL", toType="ENTREZID", OrgDb="org.Hs.eg.db")
-eg <- eg[, "ENTREZID"]
-x <- enrichPathway(gene=eg,pvalueCutoff=0.05, readable=T)
-head(x)
-
-
-x <- c("GPX3",  "GLRX",   "LBP",   "CRYAB", "DEFB1", "HCLS1",   "SOD2",   "HSPA2",
-       "ORM1",  "IGFBP1", "PTHLH", "GPC3",  "IGFBP3","TOB1",    "MITF",   "NDRG1",
-       "NR1H4", "FGFR3",  "PVR",   "IL6",   "PTPRM", "ERBB2",   "NID2",   "LAMB1",
-       "COMP",  "PLS3",   "MCAM",  "SPP1",  "LAMC1", "COL4A2",  "COL4A1", "MYOC",
-       "ANXA4", "TFPI2",  "CST6",  "SLPI",  "TIMP2", "CPM",     "GGT1",   "NNMT",
-       "MAL",   "EEF1A2", "HGD",   "TCN2",  "CDA",   "PCCA",    "CRYM",   "PDXK",
-       "STC1",  "WARS",  "HMOX1", "FXYD2", "RBP4",   "SLC6A12", "KDELR3", "ITM2B")
-eg = bitr(x, fromType="SYMBOL", toType="ENTREZID", OrgDb="org.Hs.eg.db")
-head(eg)
-
-
-
-
-#Para transformar a ENSG...(keytype es lo que tú quieres cambiar)(column a lo que quieres cambiar) CYTOSCAPE:
-entrez_ids <- AnnotationDbi::select(org.Hs.eg.db,
-                                    keys = resSIG@rownames, #Cambiar key segun analisis
+                                    keys = resSig@rownames, #Change Keys when you change the analysis
                                     keytype = "SYMBOL",
                                     columns = "ENTREZID")
-
-
-entrez_ids <- entrez_ids[!is.na(entrez_ids$ENSEMBL), ]
-columns(org.Hs.eg.db)
-keytypes(org.Hs.eg.db)
-
-write.table(cytoscape, file = "cytoscape.csv", sep = "\t", col.names = FALSE, row.names = FALSE)
-write.table(entrez_ids, file = "entrez_cytoscape.csv", sep = "\t", col.names = FALSE, row.names = FALSE)
-write.table(resSIG$pvalue, file = "pvalue_cytoscape.csv", sep = "\t", col.names = FALSE, row.names = FALSE)
+cytoscape$GeneID=entrez_ids$ENTREZID
+cytoscape$log2FC=resSig$log2FoldChange
+cytoscape$pvalue=resSig$pvalue
+cytoscape$Gene_Name <- NULL
+cytoscape <- cytoscape[!is.na(cytoscape$GeneID), ] 
+write.table(cytoscape, file = "cytoscape_ENTREZID.csv", sep = "\t", row.names = FALSE)
 
 #### KEGG pathways ####
 entrez_ids <- AnnotationDbi::select(org.Hs.eg.db,
-                                    keys = resSig_names, #Cambiar key segun analisis
+                                    keys = resSig_names, #Change Keys when you change the analysis
                                     keytype = "SYMBOL",
                                     columns = "ENTREZID")
 
-entrez_ids <- entrez_ids[ , "ENTREZID"] 
-#Voy a crear dataframe con los entrezid y los nombres de los genes
+## We create a dataframe with EntrezID and GeneName to help us to know the genename after doing enrichKEGG##
 name_entrezid <- entrez_ids
 
+entrez_ids <- entrez_ids[ , "ENTREZID"] 
 entrez_ids <- entrez_ids[!is.na(entrez_ids)] 
 KEGG_analysis <- enrichKEGG(entrez_ids,
                            organism = "hsa",
@@ -259,7 +210,6 @@ write.table(KEGG_pathways, file = "KEGG_pathways_padj.csv", row.names = FALSE)
 write.table(KEGG_pathways, file = "KEGG_pathways_pval.csv", row.names = FALSE)
 
 #### GO:BP ####
-
 GO_analysis <- function (genes, ontology){
   clusterProfiler::enrichGO(gene          = genes,
                             # universe      = universe,
@@ -270,52 +220,50 @@ GO_analysis <- function (genes, ontology){
                             pvalueCutoff  = 0.05,
                             qvalueCutoff  = 0.05)
 }
-
-GO_BP <- GO_analysis(resSig_names, "BP")
+### Biological Process ###
+GO_BP <- GO_analysis(resSIG_names, "BP")
 GO_BP_up <- GO_analysis(resSig_up_names, "BP")
 GO_BP_down <- GO_analysis(resSig_down_names, "BP")
 
-GO_MF <- GO_analysis(resSig_names, "MF")
-GO_CC <- GO_analysis(resSIG_names, "CC")
-
+## We save the results in a dataframe to be able to investigate them ###
 GO_BP_df <- as.data.frame(GO_BP@result)
 GO_BP_up_df <- as.data.frame(GO_BP_up@result)
 GO_BP_down_df <- as.data.frame(GO_BP_down@result)
 
+### Molecular Function and Cellular components ###
+GO_MF <- GO_analysis(resSig_names, "MF")
+GO_CC <- GO_analysis(resSig_names, "CC")
+
+## We save the results in a dataframe to be able to investigate them ###
 GO_MF_df <- as.data.frame(GO_MF@result)
 GO_CC_df <- as.data.frame(GO_CC@result)
 
+### We save the results in a .csv file ###
 write.table(GO_BP_df, file = "GO_BP.csv", sep = "\t", row.names = FALSE)
 write.table(GO_BP_up_df, file = "GO_BP_up.csv", sep = "\t", row.names = FALSE)
 write.table(GO_BP_down_df, file = "GO_BP_down.csv", sep = "\t", row.names = FALSE)
-
 write.table(GO_MF_df, file = "GO_MF_pval.csv", sep = "\t")
 write.table(GO_CC_df, file = "GO_CC_pval.csv", sep = "\t")
 
-#Creamos los plots en los que vemos las diferencias entre Resistente y parental
-
-  #Aqui si queremos ver diferentes colores entre lineas celulares, en color ponemos
-    #celltype, si es la misma linea celular, ponemos dex para diferenciar R de Parental
-
-geneCounts <- plotCounts(dds, gene = "DPYD", intgroup = c("dex", "celltype", "id"), returnData = TRUE)
+#We create the plots in which we can see the differences between Resistants and Sensibles ###
+geneCounts <- plotCounts(dds, gene = "RPS4Y1", intgroup = c("dex", "celltype", "id"), returnData = TRUE)
 ggplot(geneCounts, aes(x = dex, y = count, color = id)) + scale_y_log10() + geom_beeswarm(cex = 3)
 
-
-##### PARA VER EXPRESION DE UN GEN EN CADA UNA DE LAS LINEAS CELULARES #####
-d <- plotCounts(dds, gene="UPP1", intgroup="dex", returnData=TRUE)
+### To see the expression of a gene in each cell lines ###
+d <- plotCounts(dds, gene="TM4SF18", intgroup="dex", returnData=TRUE)
 d$name <- rownames(d)
 ggplot(d, aes(x=dex, y=count, color=dex)) + 
   geom_point(position=position_jitter(w=0.1,h=0)) +
   geom_text_repel(aes(label = name)) + 
   theme_bw() +
-  ggtitle("MBD4") +
+  ggtitle("Gene KRT7") +
   theme(plot.title=element_text(hjust=0.5))
 
-#### VOLCANO PLOT ####
+### VOLCANO PLOT to see easier the log2fc and the p value ###
 EnhancedVolcano(res,
                 lab = rownames(res),
                 x = 'log2FoldChange',
-                y = 'pvalue',
+                y = 'pvalue', # change if we use padj
                 title = 'TAS102 R-S',
                 pCutoff = 0.05,
                 FCcutoff = 2.5,
